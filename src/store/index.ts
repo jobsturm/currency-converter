b280817f6e814c766eaff68ec450845118245475
Job Sturm <jobsturm@gmail.com>
2021-03-31 23:50:29 +0200

    Added a few unit tests!

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,42 +1,64 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, { ActionTree, MutationTree, GetterTree } from 'vuex';
 import ForeignExchangeInterface from '@/Interfaces/ForeignExchangeInterface';
 
 Vue.use(Vuex);
 
+interface stateInterface {
+  foreignExchange: ForeignExchangeInterface|null,
+  conversionAmount: number,
+  requestError: string|null,
+}
+
+const baseState:stateInterface = {
+  foreignExchange: null,
+  // The number globally used for conversion, in Euro cents
+  conversionAmount: 0,
+  requestError: null,
+};
+
+const mutations:MutationTree<stateInterface> = {
+  setForeignExchange(state, foreignExchange):void {
+    Vue.set(state, 'foreignExchange', foreignExchange);
+  },
+  setConversionAmount(state:stateInterface, amount:number):void {
+    state.conversionAmount = amount;
+  },
+  setRequestError(state:stateInterface, errorMessage:string):void {
+    state.requestError = errorMessage;
+  },
+};
+
+const actions:ActionTree<stateInterface, stateInterface> = {
+  async getForeignExchange({ commit }):Promise<void> {
+    const response = await fetch('https://api.exchangeratesapi.io/latest');
+    if (!response.ok) {
+      commit('setRequestError', response.statusText);
+      return;
+    }
+    const foreignExchange:ForeignExchangeInterface = await response.json();
+    commit('setForeignExchange', foreignExchange);
+  },
+};
+
+const getters:GetterTree<stateInterface, stateInterface> = {
+  availableCurrencies({ foreignExchange }):Array<string> {
+    if (!foreignExchange) return [];
+    return [foreignExchange.base, ...Object.keys(foreignExchange.rates)];
+  },
+};
+
+export {
+  stateInterface,
+  baseState,
+  mutations,
+  actions,
+  getters,
+};
+
 export default new Vuex.Store({
-  state: {
-    foreignExchange: null as ForeignExchangeInterface|null,
-    // The number globally used for conversion, in Euro cents
-    conversionAmount: 0 as number,
-    requestError: null as string|null,
-  },
-  mutations: {
-    setForeignExchange(state, foreignExchange:ForeignExchangeInterface):void {
-      Vue.set(state, 'foreignExchange', foreignExchange);
-    },
-    setConversionAmount(state, amount:number):void {
-      state.conversionAmount = amount;
-    },
-    setRequestError(state, errorMessage:string):void {
-      state.requestError = errorMessage;
-    },
-  },
-  actions: {
-    async getForeignExchange({ commit }):Promise<void> {
-      const response = await fetch('https://api.exchangeratesapi.io/latest');
-      if (!response.ok) {
-        commit('setRequestError', response.statusText);
-        return;
-      }
-      const foreignExchange:ForeignExchangeInterface = await response.json();
-      commit('setForeignExchange', foreignExchange);
-    },
-  },
-  getters: {
-    availableCurrencies({ foreignExchange }):Array<string> {
-      if (!foreignExchange) return [];
-      return [foreignExchange.base, ...Object.keys(foreignExchange.rates)];
-    },
-  },
+  state: baseState,
+  mutations,
+  actions,
+  getters,
 });
--- tests/unit/Converter.spec.ts
+++ tests/unit/Converter.spec.ts
@@ -7,28 +7,85 @@ import {
 } from '@vue/test-utils';
 import Converter from '@/components/Converter.vue';
 import Vuex, { Store } from 'vuex';
+import { mutations, stateInterface, getters } from '@/store/index';
+import Vue from 'vue';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
 const availableCurrencies = ['EUR', 'CAD', 'GBP', 'USD'];
+const mockCurrencyAPI = {
+  rates: {
+    CAD: 2,
+    GBP: 4,
+    USD: 8,
+  },
+  base: 'EUR',
+  date: '2021-03-31',
+};
 
-describe('Getters.vue', () => {
-  let getters;
-  let store:Store<string>;
+describe('Converter.vue', () => {
+  let state:stateInterface;
+  let store:Store<stateInterface>;
 
   beforeEach(() => {
-    getters = {
-      availableCurrencies: () => availableCurrencies,
+    state = {
+      foreignExchange: mockCurrencyAPI,
+      conversionAmount: 5,
+      requestError: null,
     };
 
     store = new Vuex.Store({
+      state,
+      mutations,
       getters,
     });
   });
 
-  it('renders currency types when passed', () => {
+  it('Renders options currency types when passed', () => {
     const wrapper = shallowMount(Converter, { store, localVue });
-    expect(wrapper.text()).to.include(availableCurrencies.join(''));
+    wrapper.findAll('option').wrappers.forEach((optionWrapper, index) => {
+      expect(optionWrapper.text()).to.equal(availableCurrencies[index]);
+    });
+  });
+
+  it('Renders currency from conversionAmount in Store', () => {
+    const wrapper = shallowMount(Converter, { store, localVue });
+    const textInput = <HTMLInputElement><unknown>wrapper.find('input[type="number"]').element;
+    expect(textInput.value).to.equal('5');
+  });
+
+  it('Currency changes when another currency type is selected from select', async () => {
+    const wrapper = shallowMount(Converter, { store, localVue });
+    const textInput = <HTMLInputElement><unknown>wrapper.find('input[type="number"]').element;
+    const select = wrapper.find('select');
+    await select.setValue('USD');
+    expect(textInput.value).to.not.equal('5');
+  });
+
+  it('Currency converts when another currency type is selected from select', async () => {
+    const wrapper = shallowMount(Converter, { store, localVue });
+    const textInput = wrapper.find('input[type="number"]');
+    const textInputElement = <HTMLInputElement><unknown>textInput.element;
+    await textInput.setValue(5);
+    const select = wrapper.find('select');
+    await select.setValue('USD');
+    expect(textInputElement.value).to.equal((5 * mockCurrencyAPI.rates.USD).toString());
+  });
+
+  it('Calls setForeignExchange with the correct value when amount changes', async () => {
+    const wrapper = shallowMount(Converter, { store, localVue });
+    const textInput = wrapper.find('input[type="number"]');
+    await textInput.setValue(10);
+    expect(state.conversionAmount).to.equal(10 * 100);
+  });
+
+  it('foreignExchange change triggers a new conversion and data render', async () => {
+    const wrapper = shallowMount(Converter, { store, localVue });
+    store.commit('setConversionAmount', 100);
+    const textInput = wrapper.find('input[type="number"]');
+    const textInputElement = <HTMLInputElement><unknown>textInput.element;
+    await Vue.nextTick();
+    expect(textInputElement.value).to.equal((100 / 100).toString());
   });
 });
